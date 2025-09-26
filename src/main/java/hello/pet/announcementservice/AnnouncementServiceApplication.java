4fc0a95ecfee6c28f09b7306982baddadac617c6
xeimun <hinote444@naver.com>
2025-09-26 15:15:14 +0900

    :tada: 프로젝트 초기 설정
    - 기본 의존성 추가
    - Spring Boot application 클래스 생성
    - Docker Compose 파일 추가
    - 패키지 구조 생성

---
+++ src/main/java/hello/pet/announcementservice/AnnouncementServiceApplication.java
@@ -0,0 +1,13 @@
+package hello.pet.announcementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnouncementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnouncementServiceApplication.class, args);
+	}
+
+}
