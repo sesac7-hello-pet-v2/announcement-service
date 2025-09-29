0a467315bcf06cd5f39759f7c04f4a64bea00985
xeimun <hinote444@naver.com>
2025-09-29 16:46:53 +0900

    :bug: fix: @JsonFormat 추가하여 날짜 역직렬화 오류 해결

--- src/main/java/hello/pet/announcementservice/dto/request/AnnouncementUpdateRequest.java
+++ src/main/java/hello/pet/announcementservice/dto/request/AnnouncementUpdateRequest.java
@@ -1,11 +1,13 @@
 package hello.pet.announcementservice.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import hello.pet.announcementservice.entity.AnnouncementStatus;
 import java.time.LocalDateTime;
 import lombok.Data;
 
 @Data
 public class AnnouncementUpdateRequest {
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime announcementPeriod;      // 공고 기간
     private AnnouncementStatus status;             // 공고 상태
 }
