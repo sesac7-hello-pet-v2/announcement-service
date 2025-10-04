8798423a05c80f95007818dbe4e160ab01534941
xeimun <hinote444@naver.com>
2025-10-05 05:43:17 +0900

    :sparkles: feat: application-service에서 공고 상태 완료 처리 API를 호출할 수 있도록 엔드포인트 추가

--- src/main/java/hello/pet/announcementservice/controller/AnnouncementController.java
+++ src/main/java/hello/pet/announcementservice/controller/AnnouncementController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -64,4 +65,11 @@ public class AnnouncementController {
         announcementService.deleteAnnouncement(id, shelterId);
         return ResponseEntity.noContent().build();
     }
+
+    @PatchMapping("/{id}/complete")
+    public ResponseEntity<Void> completeAnnouncement(@PathVariable Long id) {
+        announcementService.completeAnnouncement(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
