04ac64f1e53b19f0868de8ecc734bef29e22701f
xeimun <hinote444@naver.com>
2025-10-05 03:27:07 +0900

    :wrench: chore: 공고 API 경로에 v1 버전 추가

--- src/main/java/hello/pet/announcementservice/controller/AnnouncementController.java
+++ src/main/java/hello/pet/announcementservice/controller/AnnouncementController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/announcements")
+@RequestMapping("/v1/announcements")
 @RequiredArgsConstructor
 public class AnnouncementController {
 
