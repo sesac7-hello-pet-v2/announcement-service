46799a7c91cbdaccd8795ac2f7a7b2c62fb169a6
xeimun <hinote444@naver.com>
2025-10-13 18:08:36 +0900

    :recycle: refactor: UserService 클라이언트에 url, path 속성 추가

--- src/main/java/hello/pet/announcementservice/service/client/UserServiceClient.java
+++ src/main/java/hello/pet/announcementservice/service/client/UserServiceClient.java
@@ -4,8 +4,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service")
+@FeignClient(
+        name = "user-service",
+        url = "http://user-service:8082",
+        path = "/internal/v1/users"
+)
 public interface UserServiceClient {
-    @GetMapping("/internal/users/{id}/nickname")
+
+    @GetMapping("/{id}/nickname")
     String getNickname(@PathVariable("id") Long userId);
 }
