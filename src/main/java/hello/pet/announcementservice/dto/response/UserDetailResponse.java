be76732628f81e47398f44a85d6fdd30ea4b9d38
xeimun <hinote444@naver.com>
2025-10-14 10:56:32 +0900

    :bug: fix: 역직렬화를 위해 기본 생성자 추가

--- src/main/java/hello/pet/announcementservice/dto/response/UserDetailResponse.java
+++ src/main/java/hello/pet/announcementservice/dto/response/UserDetailResponse.java
@@ -1,8 +1,12 @@
 package hello.pet.announcementservice.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDetailResponse {
     private String email;
     private String nickname;
